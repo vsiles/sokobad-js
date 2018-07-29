df1b6125d43bacbb1dcc0bbad806179339157e08
Vincent Siles <vincent.siles@ens-lyon.org>
2018-07-29 11:03:26 +0200

    static drawing of the map

--- main.js
+++ main.js
@@ -2,40 +2,60 @@
 
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
+const CELL_SIZE = 32;
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+function draw_cell(x, y, size, color)
+{
+    let posx = x * size;
+    let posy = y * size;
+    ctx.beginPath();
+    ctx.rect(posx, posy, size, size);
+    ctx.fillStyle = color;
+    ctx.fill();
+    ctx.closePath();
+}
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
-
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath();
-
+let map_width;
+let map_height;
+let map_data;
 
 function process_map(map)
 {
-    let width = map.width;
-    let height = map.height;
-    let data = map.map;
-    console.log(`map.width = ${width}`);
-    console.log(`map.height = ${height}`);
-    for (let i = 0; i < height; i++) {
-        let line = data[i];
-        console.log(`  ${line}`);
+    map_width = map.width;
+    map_height = map.height;
+    map_data = map.map;
+    for (let i = 0; i < map_height; i++) {
+        let line = map_data[i];
+        map_data[i] = line;
     }
 }
 
-$.getJSON('data/maps/map0.json', process_map
+function draw_map()
+{
+    for (let j = 0; j < map_height; j++) {
+        for (let i = 0; i < map_width; i++) {
+            let cell = map_data[j][i];
+            let color = "#C0C0C0";
+            switch (cell) {
+                case '.': color = "#606060"; break;
+                case 'b': color = "#663300"; break;
+                case 'g': color = "#FFFF33"; break;
+                case 'x': color = "#000000"; break;
+                case 's': color = "#FF0000"; break; // TEMP
+            }
+            draw_cell(i, j, CELL_SIZE, color);
+        }
+    }
+}
+
+
+function main_loop(json_data)
+{
+    process_map(json_data);
+    draw_map();
+}
+
+$.getJSON('data/maps/map0.json', main_loop
 ).done(function () {
     console.log("map0 loaded successfully");
     }
